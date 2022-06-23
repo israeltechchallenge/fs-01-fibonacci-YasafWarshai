3d97f4d92fac5859461bd727cf234065725f6802
YasafWarshai <warshy228@gmail.com>
2022-06-23 22:30:09 +0300

    Milestone 5

--- fibonacci.js
+++ fibonacci.js
@@ -1,26 +1,42 @@
-
-
 let getNumber = document.getElementById("numberHere");
-let fibURL = "http://localhost:5050/fibonacci/"
-let answer = document.getElementById("answer")
+let fibURL = "http://localhost:5050/fibonacci/";
+let answer = document.getElementById("answer");
+let mySpinner = document.getElementById("spinner")
 
-function getFib(){
-  console.log(`${fibURL}${getNumber.value}`)
-fetch(`${fibURL}${getNumber.value}`)
-  .then(function (response) {
-    
-return response.json() })
-.then (function (data){
- answer.innerHTML = (data.result)
- 
+function getFib() {
+  mySpinner.classList.remove("visually-hidden");
+  answer.classList.add("visually-hidden");
+
+  console.log(`${fibURL}${getNumber.value}`);
+
+  fetch(`${fibURL}${getNumber.value}`)
+    .then(function (response) {
+      if (!response.ok) {
+        response.text().then((error) => {
+          answer.classList.remove("visually-hidden");
+          answer.classList.add("text-danger");
+          getNumber.classList.add("border-danger");
+          answer.innerText = error;
+          mySpinner.classList.toggle("visually-hidden");
+          console.log(error);
+        });
+      }
+      return response.json();
+    })
+
+    .then((data) => {
+      answer.innerHTML = data.result;
+    })
+
+    .then((spinner) => {
+     mySpinner.classList.toggle("visually-hidden");
+      answer.classList.remove("visually-hidden");
+      answer.classList.remove("text-danger");
+      getNumber.classList.remove("border-danger");
+    });
 }
-)
-}
 
-
-document.getElementById("submitButton").addEventListener("click", getFib)
-
-
+document.getElementById("submitButton").addEventListener("click", getFib);
 
 /* function fibonacci(e){
   e.preventDefault();
@@ -39,4 +55,3 @@ return y;
 
 document.getElementById("submitButton").addEventListener("click", fibonacci)
  */
-
