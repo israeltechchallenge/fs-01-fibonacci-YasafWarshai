d6761ece3673cb0eb4029b7cd05c7e8254583455
YasafWarshai <warshy228@gmail.com>
2022-06-23 15:40:07 +0300

    milestone 4

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,28 @@
 
-function fibonacci(e){
+
+let getNumber = document.getElementById("numberHere");
+let fibURL = "http://localhost:5050/fibonacci/"
+let answer = document.getElementById("answer")
+
+function getFib(){
+  console.log(`${fibURL}${getNumber.value}`)
+fetch(`${fibURL}${getNumber.value}`)
+  .then(function (response) {
+    
+return response.json() })
+.then (function (data){
+ answer.innerHTML = (data.result)
+ 
+}
+)
+}
+
+
+document.getElementById("submitButton").addEventListener("click", getFib)
+
+
+
+/* function fibonacci(e){
   e.preventDefault();
   let num = document.getElementById("numberHere").value;
   let x = 0, y = 1, c;
@@ -10,9 +33,10 @@ function fibonacci(e){
   y = c;
 
 }
-document.getElementById("answer").innerHTML = y;
 
 return y;
 }
 
 document.getElementById("submitButton").addEventListener("click", fibonacci)
+ */
+
